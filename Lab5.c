4e78bae5e791c255c524b8185c5d7b1d6883de0a
Davis Samuel <dsamuel5733@gmail.com>
2017-09-28 11:29:03 -0400

    Add files via upload

---
+++ Lab5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int disk[256];
+    int memory[16];
+    int cache[4];
+
+    int i;
+    for (i = 0; i < 256; i++) {
+        disk[i] = i;
+    }
+
+    return (0);
+}
+
