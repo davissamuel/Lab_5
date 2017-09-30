4ef2187315ce2151e2727de72d73784fa935d420
Davis Samuel <dsamuel5733@gmail.com>
2017-09-29 20:03:28 -0400

    Added an if statement for every situation

--- Lab5.c
+++ Lab5.c
@@ -44,15 +44,42 @@ int main() {
 	int memIndex = value % memSize;
 
 	
+    //This if statement runs if the cache[cacheIndex] is empty (Compulsory miss)
 	if(cache[cacheIndex] == 0){
-	printf("\nMiss: ");
-		if(memory[memIndex]==0){
+        printf("\nCompulsory​ Miss: NULL");
+        cache[cacheIndex] = disk[value]; //Takes the content of the disk[value] and copies it into cache[cacheIndex]
+ 		if(memory[memIndex] != value){ //If the memory[memIndex] is empty OR it contains a value != to the value entered by the user
 			printf("Not in memory\n");
+            memory[memIndex] = disk[value]; //Takes the content of the disk[value] and copies it into memory[memIndex]
 		}
-		else
+		else //If the value entered is in memory[memIndex]
 			printf("In memory\n");
-	}
+    }
+    
+    //This if statement runs if the cache[cacheIndex] is a value that is != to the value entered by the user (Conflict miss)
+    else if ((cache[cacheIndex] != value) && (cache[cacheIndex] != 0)){
+        printf("\Conflict​ Miss: %d", cache[cacheIndex]);
+        cache[cacheIndex] = disk[value];
+        if(memory[memIndex] != value){
+            printf("Not in memory\n");
+            memory[memIndex] = disk[value];
+        }
+        else
+            printf("In memory\n");
+    }
+    
+    //This if statement runs if the cache[cacheIndex] is equal to the value entered
+    else {
+        printf("Hit: %d", cache[cacheIndex]);
+        if(memory[memIndex] != value){
+            printf("Not in memory\n");
+            memory[memIndex] = disk[value];
+        }
+        else
+            printf("In memory\n");
+    }
 
+    
 	double cacheAccess = cacheSize / 4;
 	double memAccess = 100 * (memSize / 16);
 	double diskAccess = 100000 * (diskSize / 256);
